a2db4cc5062fac09053403763adc8cbce04ad0c7
Marcos Melero <mmelero@gmail.com>
2021-02-15 12:31:52 -0300

    Banco de dados H2 e criação automática da base de dados/Mapeamento da classe Categoria

--- src/main/java/com/mmelero/cursomc/domain/Categoria.java
+++ src/main/java/com/mmelero/cursomc/domain/Categoria.java
@@ -2,9 +2,21 @@ package com.mmelero.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import net.bytebuddy.implementation.bytecode.assign.reference.GenericTypeAwareAssigner;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	//Geração automatica dos ids da tabela Categoria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
