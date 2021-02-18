b0ca2d7997b36d9c6bf9bd6ede52f06d20888e25
Marcos Melero <mmelero@gmail.com>
2021-02-17 21:02:42 -0300

    pageinação com parametros opicionais na requisição

--- src/main/java/com/mmelero/cursomc/services/CategoriaService.java
+++ src/main/java/com/mmelero/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.mmelero.cursomc.domain.Categoria;
@@ -51,6 +54,12 @@ public class CategoriaService {
 		
 		return repo.findAll();
 		
+	}
+	
+	//função para criar paginação
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page,linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
