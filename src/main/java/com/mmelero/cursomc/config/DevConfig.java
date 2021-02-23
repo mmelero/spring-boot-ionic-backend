53d910b3c28ba654a48a4fbc0c6d8af65cef2bbe
Marcos Melero <mmelero@gmail.com>
2021-02-23 11:00:42 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/mmelero/cursomc/config/DevConfig.java
+++ src/main/java/com/mmelero/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.mmelero.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,14 +11,22 @@ import org.springframework.context.annotation.Profile;
 import com.mmelero.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestCinfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
 	
+	//pega o valor do atributo dentro application-dev.properties para compara se 
+	//o valor é create - crirar o bco de dados todas vezes que inicia
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
 		dbService.instantiateDatabase();
 		return true;
 	}
