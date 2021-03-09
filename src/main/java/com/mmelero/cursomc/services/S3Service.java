063f5b3a32f57be437f5b207a1b307cfeb1907d1
Marcos Melero <mmelero@gmail.com>
2021-03-09 09:20:26 -0300

    Tratando exceções adequadamente

--- src/main/java/com/mmelero/cursomc/services/S3Service.java
+++ src/main/java/com/mmelero/cursomc/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.mmelero.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -35,7 +36,7 @@ public class S3Service {
 			String contentType = multipartFile.getContentType();
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro ao IO: "+e.getMessage());
+			throw new FileException("Erro ao IO: "+e.getMessage());
 		}
 
 	}
@@ -48,7 +49,7 @@ public class S3Service {
 			s3client.putObject(bocketName, contentType, is, meta);
 			return s3client.getUrl(bocketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL em URI");
+			throw new FileException("Erro ao converter URL em URI");
 		}
 	}
 }
